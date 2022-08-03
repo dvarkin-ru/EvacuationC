d9d071b380ab17a61df49e42740c5d016d6358d9
NikBel3476 <nikita.belkin3476@gmail.com>
2022-08-03 19:37:39 +0400

    feature(parsing): add json deserialize for buildings

--- src/configuration/src/lib.rs
+++ src/configuration/src/lib.rs
@@ -7,79 +7,81 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-enum DistributionType {
+pub enum DistributionType {
 	FromBim,
 	Uniform
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-enum TransitionType {
+pub enum TransitionType {
 	FromBim,
 	Users
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct DistributionSpecial {
-	uuid: Vec<String>,
-	density: f64,
+pub struct DistributionSpecial {
+	pub uuid: Vec<String>,
+	pub density: f64,
 	#[serde(rename="_comment")]
-	comment: String
+	pub comment: String
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct TransitionSpecial {
-	uuid: Vec<String>,
-	width: f64,
+pub struct TransitionSpecial {
+	pub uuid: Vec<String>,
+	pub width: f64,
 	#[serde(rename="_comment")]
-	comment: String
+	pub comment: String
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Distribution {
+pub struct Distribution {
 	#[serde(rename="type")]
-	distribution_type: DistributionType,
-	density: f64,
-	special: Vec<DistributionSpecial>
+	pub distribution_type: DistributionType,
+	pub density: f64,
+	pub special: Vec<DistributionSpecial>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Transition {
+pub struct Transition {
 	#[serde(rename="type")]
-	transitions_type: TransitionType,
+	pub transitions_type: TransitionType,
 	#[serde(rename="doorwayin")]
-	doorway_in: f64,
+	pub doorway_in: f64,
 	#[serde(rename="doorwayout")]
-	doorway_out: f64,
-	special: Vec<TransitionSpecial>
+	pub doorway_out: f64,
+	pub special: Vec<TransitionSpecial>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Modeling {
-	step: f64,
+pub struct Modeling {
+	pub step: f64,
 	#[serde(rename="speed_max")]
-	max_speed: f64,
+	pub max_speed: f64,
 	#[serde(rename="density_max")]
-	max_density: f64,
+	pub max_density: f64,
 	#[serde(rename="density_min")]
-	min_density: f64
+	pub min_density: f64
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ScenarioCfg {
 	#[serde(rename="bim")]
-	files: Vec<String>,
-	distribution: Distribution,
+	pub files: Vec<String>,
+	pub distribution: Distribution,
 	#[serde(rename="transits")]
-	transition: Transition,
-	modeling: Modeling
+	pub transition: Transition,
+	pub modeling: Modeling
 }
 
-pub fn load_cfg (filename: &String) -> Result<ScenarioCfg, Box<dyn Error>> {
-	let json_content = fs::read_to_string(filename)
-		.expect("Не удалось прочитать файл конфигурации сценария");
+pub fn load_cfg (path_to_file: &String) -> Result<ScenarioCfg, Box<dyn Error>> {
+	let json_content = fs::read_to_string(path_to_file).unwrap_or_else(|err| {
+		eprintln!("Ошибка чтения файла конфигурации сценария {}: {}", path_to_file, err);
+		process::exit(1);
+	});
 	let cfg: ScenarioCfg = serde_json::from_str(&json_content).unwrap_or_else(|err| {
-		println!("Ошибка чтения файла конфигурации сценария: {}", err);
+		eprintln!("Ошибка десериализации файла конфигурации сценария {}: {}", path_to_file, err);
 		process::exit(1);
 	});
 
