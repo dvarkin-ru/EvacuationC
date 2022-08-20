c50a31e5d0479beee8e7c29416887d7dcd56cff8
NikBel3476 <nikita.belkin3476@gmail.com>
2022-08-20 14:16:24 +0400

    feature: add event listener to file input

--- src-ui/main.js
+++ src-ui/main.js
@@ -2,3 +2,9 @@ import { invoke } from "@tauri-apps/api";
 
 invoke('greet', { name: 'World' })
 	.then(response => console.log(response));
+
+const fileInput = document.getElementById('file_input');
+
+fileInput.addEventListener('change', e => {
+	console.log(e.target.value);
+});
