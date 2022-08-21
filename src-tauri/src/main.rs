5449bdf3e961f97c1a5c99fd02ecd71eca6fe9ce
NikBel3476 <nikita.belkin3476@gmail.com>
2022-08-21 20:38:32 +0400

    feature: add button handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,28 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
+use std::fs;
+use cli;
+use configuration;
+use json_object;
+use bim_cli;
+use bim_configure;
+use bim_json_object;
+
+extern "C" {
+	fn run();
+}
+
 fn main() {
 	tauri::Builder::default()
-		.invoke_handler(tauri::generate_handler![greet])
+		.invoke_handler(tauri::generate_handler![start])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-	format!("Hello, {}!", name)
+fn start() {
+	fs::create_dir_all("result").expect("Не удалось создать папку result");
+	unsafe { run() }
+	println!("Hello from Rust!");
 }
--- src-ui/main.js
+++ src-ui/main.js
@@ -3,8 +3,10 @@ import { invoke } from "@tauri-apps/api";
 invoke('greet', { name: 'World' })
 	.then(response => console.log(response));
 
-const fileInput = document.getElementById('file_input');
-
-fileInput.addEventListener('change', e => {
+document.getElementById('file_input').addEventListener('change', e => {
 	console.log(e.target.value);
+});
+
+document.getElementById('start-btn').addEventListener('click', _ => {
+	invoke('start').then(_ => alert('started'));
 });
