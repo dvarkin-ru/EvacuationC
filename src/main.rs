886af95ff065720f3d23414aeda941b7ca5c058b
NikBel3476 <nikita.belkin3476@gmail.com>
2022-08-21 18:16:27 +0400

    feature: add run.h

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ use bim_cli;
 use bim_configure;
 use bim_json_object;
 
-extern "C" {
-	fn run();
-}
+mod run_bindings;
 
 fn main() {
 	fs::create_dir_all("result").expect("Не удалось создать папку result");
-	unsafe { run() }
+	unsafe { run_bindings::run() }
 }
---
+++ src/run.h
@@ -0,0 +1,10 @@
+//
+// Created by nikit on 21.08.2022.
+//
+
+#ifndef RUN_H
+#define RUN_H
+
+#endif //RUN_H
+
+void run();
