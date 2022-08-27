b8b68e4b4fa3340f3648d6d0416f70a435e68d95
NikBel3476 <nikita.belkin3476@gmail.com>
2022-08-27 21:58:20 +0400

    feature(ui): update packages
    React was removed

--- src-ui/App.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-
-const App: FC = () => {
-	return <h3>Главная страница</h3>;
-};
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 export default defineConfig({
-	plugins: [react()],
 	// prevent vite from obscuring rust errors
 	clearScreen: false,
 	// Tauri expects a fixed port, fail if that port is not available
